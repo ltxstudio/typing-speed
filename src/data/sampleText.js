a0808a77750612a47c280e54433fcc7f4cae0a79
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-27 21:28:49 +0600

    Create sampleText.js

---
+++ src/data/sampleText.js
@@ -0,0 +1,5 @@
+export const sampleTexts = [
+  "The quick brown fox jumps over the lazy dog.",
+  "Vite is a build tool that aims to provide a faster and leaner development experience for modern web projects.",
+  "React is a JavaScript library for building user interfaces."
+];
